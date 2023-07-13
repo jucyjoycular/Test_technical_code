9e9c64986c529724dd60284d576669f53246abd2
jucyjoycular <andre2fez@gmail.com>
2023-07-13 10:26:51 +0700

    test code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useRef, useEffect, useState } from 'react';
+
 
 function App() {
+  const inputReff = useRef(null)
+  const [message, setMesage] = useState("");
+  const [updated, setUpdated] = useState("");
+  
+  useEffect(() => {
+    fetch("http://localhost:8000/message")
+    .then((res)=> res.json())
+    .then((data)=> setMesage(data.message));
+  }, []);
+
+
+  const Ganjil = () => {
+    setUpdated(inputReff.current.value)
+  };
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div>
+
+            <input type="text" name="bil" ref={inputReff} placeholder="Masukkan Angka"/>
+
+        </div>
+
+        <div>
+
+          <button>Generate Segitiga</button>
+
+        </div>
+
+        <div>
+
+          <button onClick={this.onChange}>Generate Bilangan Ganjil</button>
+
+        </div>
+
+        <div>
+
+          <button>Generate Bilangan Prima</button>
+
+        </div>
+
+        <h1>Result</h1>
+        {message}<br/>
+        
+        {updated}
       </header>
     </div>
   );
